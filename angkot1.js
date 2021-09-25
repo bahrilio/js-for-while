f992d87f3f8fe25490fa61e47f5100f69c67c80e
bahrilio <mbahrililmi2@gmail.com>
2021-09-25 20:37:21 +0700

    for/while

---
+++ angkot1.js
@@ -0,0 +1,16 @@
+// for(var angkot = 1; angkot <= 10; angkot++){
+//     console.log('angkot ini no '+ angkot +' ini');
+// }
+
+var jmlAngkot = 10,
+    angkotBeroperasi = 6,
+    noAngkot = 1;
+
+while(noAngkot <= angkotBeroperasi){
+    console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
+    noAngkot++;
+}
+
+for(noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
+    console.log('Angkot No. '+ noAngkot +' sedang tidak beroperasi');
+}
